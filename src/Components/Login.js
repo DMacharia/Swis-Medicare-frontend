20bb099614d9c3b744b7d97faf5162a79381391a
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2023-01-03 16:11:49 +0300

    modules updated

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,82 +1,21 @@
-import React, { useState } from "react";
+import React from 'react';
 
-import Form from "react-bootstrap/Form";
-
-import Button from "react-bootstrap/Button";
-
-import "./Login.css";
-
-export default function Login() {
-
-  const [email, setEmail] = useState("");
-
-  const [password, setPassword] = useState("");
-
-  function validateForm() {
-
-    return email.length > 0 && password.length > 0;
-
-  }
-
-  function handleSubmit(event) {
-
-    event.preventDefault();
-
-  }
-
-  return (
-
-    <div className="Login">
-
-      <Form onSubmit={handleSubmit}>
-
-        <Form.Group size="lg" controlId="email">
-
-          <Form.Label>Email</Form.Label>
-
-          <Form.Control
-
-            autoFocus
-
-            type="email"
-
-            value={email}
-
-            onChange={(e) => setEmail(e.target.value)}
-
-          />
-
-        </Form.Group>
-
-        <Form.Group size="lg" controlId="password">
-
-          <Form.Label>Password</Form.Label>
-
-          <Form.Control
-
-            type="password"
-
-            value={password}
-
-            onChange={(e) => setPassword(e.target.value)}
-
-          />
-
-        </Form.Group>
-
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-
-          Login
-
-        </Button>
-
-      </Form>
-
-    </div>
-
-  );
-
+function Login() {
+  return(
+    <form>
+      <label>
+        <p>Username</p>
+        <input type="text" />
+      </label>
+      <label>
+        <p>Password</p>
+        <input type="password" />
+      </label>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  )
 }
-     
 
- 
+export default Login;
