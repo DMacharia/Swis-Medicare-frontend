dd1287a0a09d8d0ba9021edb3f1d21a2969d21b4
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2023-01-07 22:46:08 +0300

    signup component deleted

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import Login from './login';
 import Patient from './patient';
-import Signup from './signup';
+import SignUp from './SignUp';
 
-function login(username, password) {
-  if (username === 'patient' && password === 'password') {
-    return true;
-  }
-  return false;
+function App() {
+
+  const [user, setUser] = useState(null);
+
+
+  useEffect(() => {
+    // auto-login
+    fetch('/me').then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+return (
+  <>
+    <NavBar user={user} setUser={setUser} />
+    <Switch>
+      <Route exact path='/me'>
+        {user ? <Fruit user={user} /> : <Login onLogin={setUser} />}
+      </Route>
+      <Route exact path='/login'>
+        {user ? <Fruit user={user} /> : <Login onLogin={setUser} />}
+      </Route>
+      <Route exact path='/signup'>
+        {user ? <Fruit user={user} /> : <SignUp onLogin={setUser} />}
+      </Route>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+    </Switch>
+  </>
+);
 }
+
   
 export default (App);
 
