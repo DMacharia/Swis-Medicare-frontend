a364e0434bb46b5394000d7f17c323ddec590e78
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2023-01-08 21:27:08 +0300

    login code updated

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,41 +1,12 @@
 import React from 'react';
 import Login from './login';
 import Patient from './patient';
-import SignUp from './SignUp';
+
 
 function App() {
 
-  const [user, setUser] = useState(null);
+  
 
-
-  useEffect(() => {
-    // auto-login
-    fetch('/me').then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
-
-return (
-  <>
-    <NavBar user={user} setUser={setUser} />
-    <Switch>
-      <Route exact path='/me'>
-        {user ? <Fruit user={user} /> : <Login onLogin={setUser} />}
-      </Route>
-      <Route exact path='/login'>
-        {user ? <Fruit user={user} /> : <Login onLogin={setUser} />}
-      </Route>
-      <Route exact path='/signup'>
-        {user ? <Fruit user={user} /> : <SignUp onLogin={setUser} />}
-      </Route>
-      <Route exact path='/'>
-        <Home />
-      </Route>
-    </Switch>
-  </>
-);
 }
 
   
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,21 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
-  return(
-    <form>
-      <label>
-        <p>Username</p>
-        <input type="text" />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // Here you would call your backend API to authenticate the user
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const { token } = await response.json();
+
+      // If login is successful, store the token in the browser's session storage
+      sessionStorage.setItem('token', token);
+
+      // Redirect the user to the home page
+      window.location.href = '/';
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username:</label>
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <br />
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <button type="submit">Login</button>
     </form>
-  )
+  );
 }
 
 export default Login;
+
