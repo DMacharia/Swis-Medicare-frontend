aab2345bcfdf288ae239eb8ff2b9afc35401550b
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2023-01-10 17:33:11 +0300

    Appointment component deleted

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from './Login';
+import Appointment from './Appointment';
 import Patient from './Patient';
 import { Route, Router } from 'react-router-dom';
 import {BrowserRouter} from 'react-router-dom';
