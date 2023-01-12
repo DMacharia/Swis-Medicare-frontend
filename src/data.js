aa5a7ef3fbf75f167cbbd5aee3b4ad5e084f4e86
D-Micheni <dennis.micheni@student.moringaschool.com>
2023-01-12 09:57:07 +0300

    src/data.js

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const Patientsdata = [
+  {
+    id: 1,
+    name: "Enock Mokua",
+  },
+  {
+    id: 2,
+    name: "John Doe",
+  },
+  {
+    id: 3,
+    name: "Julius Midega",
+  },
+  {
+    id: 4,
+    name: "Mercy Mosoti",
+  },
+  {
+    id: 5,
+    name: "Hata Shata",
+  },
+  {
+    id: 6,
+    name: "Steven Mutisya",
+  },
+];
