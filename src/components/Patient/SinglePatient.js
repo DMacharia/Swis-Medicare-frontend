a116eb009423a5e637bf0084ec0e628a26bbb008
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2023-01-17 23:29:14 +0300

    add styles to single patient compenent

--- src/components/Patient/SinglePatient.js
+++ src/components/Patient/SinglePatient.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import "./singlepatient.css";
+
 
 const singlePatientAPI = "http://127.0.0.1:3000/api/v1/patients/7";
 
@@ -12,32 +14,34 @@ const SinglePatient = () => {
   }, []);
 
   return (
-    <div>
-      <h2>Welcome!</h2>
-      <div>{patient.username}</div>
+    <div className="patient-card">
+      <div className="patient-name">
+        <h3>Welcome!</h3>
+        {patient.username}
+      </div>
       <span>
-        {patient.first_name} {patient.second_name}
+        Name: {patient.first_name} {patient.second_name}
       </span>
-      <div>{patient.email}</div>
-      <div>{patient.mobile_no}</div>
-      <div>{patient.address}</div>
-      <div>{patient.gender}</div>
-      <div>{patient.blood_group}</div>
-      <div>{patient.date_of_birth}</div>
+      <div>Email: {patient.email}</div>
+      <div>Mobile_no: {patient.mobile_no}</div>
+      <div>Address: {patient.address}</div>
+      <div>Gender: {patient.gender}</div>
+      <div>Blood_group: {patient.blood_group}</div>
+      <div>Date_of_birth: {patient.date_of_birth}</div>
       <hr />
       <h3>Patient Histories</h3>
       {patient.patient_histories &&
         patient.patient_histories.map((history) => (
-          <div key={history.id}>
-            <div>{history.blood_pressure}</div>
-            <div>{history.blood_sugar}</div>
-            <div>{history.diagnosis}</div>
-            <div>{history.medication}</div>
-            <div>{history.notes}</div>
-            <div>{history.pulse_rate}</div>
-            <div>{history.temperature}</div>
-            <div>{history.test}</div>
-            <div>{history.weight}</div>
+          <div key={history.id} className="patient-history">
+            <div>Blood_pressure: {history.blood_pressure}</div>
+            <div>Blood_sugar: {history.blood_sugar}</div>
+            <div>Diagnosis: {history.diagnosis}</div>
+            <div>Medication: {history.medication}</div>
+            <div>Notes: {history.notes}</div>
+            <div>Pulse Rate: {history.pulse_rate}</div>
+            <div>Temperature: {history.temperature}</div>
+            <div>Test: {history.test}</div>
+            <div>Weight: {history.weight}</div>
           </div>
         ))}
     </div>
