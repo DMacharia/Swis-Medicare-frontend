e9cc4383b3c729d22d5d62ffe1baeb4b4e4699f6
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2023-01-18 22:42:00 +0300

    get users profile

--- src/components/doctor/MyProfile.js
+++ src/components/doctor/MyProfile.js
@@ -56,27 +56,61 @@ const MyProfile = () => {
   const [patientFormVisible, setPatientFormVisible] = useState(false);
   return (
     <div>
-      
       <p>{userID}</p>
-
 
       <p>Hello {fake?.username}</p>
 
-     
-
-      <div className='doctor_profile'>
-        <h2>{fake.role}</h2>
+      <div className="doctor_profile">
+        <h2>{fake?.role}</h2>
       </div>
-
-      <div className='buttons'>
-              <button
-                  onClick={() => setPatientFormVisible(!patientFormVisible)} >
-                  {patientFormVisible ? 'Hide This Form' : 'Admit Patient'}
-              </button>
-              {patientFormVisible ? <CreatePatient /> : null}
+      {fake?.role === "Admin" && (
+        <>
+          <div>{fake?.username}</div>
+          <div>
+            {fake?.first_name} {fake?.second_name}
           </div>
+          <div>{fake?.email}</div>
+          <div>{fake?.mobile_no}</div>
+          <div>{fake?.address}</div>
+          <div>{fake?.title}</div>
+        </>
+      )}
+      {fake?.role === "Doctor" ? (
+        <>
+          <div>{fake?.username}</div>
+          <div>
+            {fake?.first_name} {fake?.second_name}
+          </div>
+          <div>{fake?.email}</div>
+          <div>{fake?.mobile_no}</div>
+          <div>{fake?.reg_no}</div>
+          <div>{fake?.emergency_no}</div>
+          <div>{fake?.address}</div>
+          <div>{fake?.speciality}</div>
+        </>
+      ) : (
+        <>
+          <div>{fake?.username}</div>
+          <div>
+            {fake?.first_name} {fake?.second_name}
+          </div>
+          <div>{fake?.email}</div>
+          <div>{fake?.gender}</div>
+          <div>{fake?.mobile_no}</div>
+          <div>{fake?.address}</div>
+          <div>{fake?.blood_group}</div>
+          <div>{fake?.date_of_birth}</div>
+        </>
+      )}
+
+      <div className="buttons">
+        <button onClick={() => setPatientFormVisible(!patientFormVisible)}>
+          {patientFormVisible ? "Hide This Form" : "Admit Patient"}
+        </button>
+        {patientFormVisible ? <CreatePatient /> : null}
       </div>
-  )
+    </div>
+  );
 }
 
 export default MyProfile
