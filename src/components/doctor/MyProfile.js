64114cbbce5de3d0bff4f80c85ede21acebf36e4
Salvato Luice <salvatoluice5@gmai.com>
2023-01-18 21:44:45 +0300

    add profile

--- src/components/doctor/MyProfile.js
+++ src/components/doctor/MyProfile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import CreatePatient from './CreatePatient';
@@ -9,6 +9,7 @@ const MyProfile = () => {
 
   const [user, loading, error] = useAuthState(auth);
   const [name, setName] = useState("");
+  const [fake, setFake] = useState([]);
   const navigate = useNavigate();
   const fetchUserName = async () => {
     try {
@@ -22,9 +23,34 @@ const MyProfile = () => {
     }
   };
 
+  const userID = user?.email;
+
+//   useEffect(() => {
+//     fetch("https://swis-medicare-eblx.onrender.com/api/v1/users")
+//         .then((response) => response.json())
+//         .then((data) => {
+//             console.log(data);
+//             const userEmail = "bob@example.com"
+//             const userProfile = data.filter(patient => patient.email === userEmail)[0];
+//             setProfile(userProfile);
+//         });
+// }, []);
+
+  useEffect(() => {
+    fetch("https://swis-medicare-eblx.onrender.com/api/v1/users")
+    .then(res => res.json())
+    .then(data => {
+      const userEmail = userID
+      const userProfile = data.filter(user => user.email === userEmail)[0];
+      setFake(userProfile);
+    })
+  })
+
   fetchUserName();
 
-  const userID = user?.email;
+  // console.log(fake)
+
+  
 
 
   const [patientFormVisible, setPatientFormVisible] = useState(false);
@@ -32,6 +58,15 @@ const MyProfile = () => {
     <div>
       
       <p>{userID}</p>
+
+
+      <p>Hello {fake?.username}</p>
+
+     
+
+      <div className='doctor_profile'>
+        <h2>{fake.role}</h2>
+      </div>
 
       <div className='buttons'>
               <button
