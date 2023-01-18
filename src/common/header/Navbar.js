30b10f9681b45149afe62c0f59af705b308e6275
Daniel Macharia <danielhuios@gmail.com>
2023-01-18 12:35:54 +0300

    modified Navbar to make routing dynamic

--- src/common/header/Navbar.js
+++ src/common/header/Navbar.js
@@ -7,8 +7,8 @@ import { useAuthState } from "react-firebase-hooks/auth";
 const Navbar = () => {
 
   const [MobileMenu, setMobileMenu] = useState(false)
-  const [patients, setPatients] = useState([]);
-  const [doctors, setDoctors] = useState([]);
+  const [patientsMail, setPatientsMail] = useState([]);
+  const [doctorsMail, setDoctorsMail] = useState([]);
 
   const [user, loading, error] = useAuthState(auth);
   const [name, setName] = useState("");
@@ -35,14 +35,20 @@ const Navbar = () => {
   useEffect(() => {
 		fetch("https://swis-medicare-eblx.onrender.com/api/v1/patients")
 			.then((response) => response.json())
-			.then((data) => setPatients(data));
-	});
+			.then((data) => {
+                setPatientsMail(data.map(patient => patient.email))
+                console.log(patientsMail)
+            });
+	},[]);
 
   useEffect(() => {
 		fetch("https://swis-medicare-eblx.onrender.com/api/v1/doctors")
 			.then((response) => response.json())
-			.then((data) => setDoctors(data));
-	});
+			.then((data) => {
+                setDoctorsMail(data.map(doctor => doctor.email))
+                console.log(doctorsMail)
+            });
+	},[]);
 
 
   return (
@@ -66,7 +72,7 @@ const Navbar = () => {
               </li>
               <li>
                 {!user ? <Link to='/login'>Login</Link> : (
-                  user?.email === "peterparker@gmail.com" ? <Link to='/adminhome' style={{color: '#0f3460', fontWeight: 'bold', fontSize: '22px'}}>Admin Page</Link> : (user?.email === "bob@example.com" ? <Link style={{color: '#0f3460', fontWeight: 'bold', fontSize: '22px'}}>Patient</Link> : (user?.email === "john@example.com" ? <Link style={{color: '#0f3460', fontWeight: 'bold', fontSize: '22px'}}>Doctor</Link> : null))
+                  user?.email === "peterparker@gmail.com" ? <Link to='/adminhome' style={{color: '#0f3460', fontWeight: 'bold', fontSize: '22px'}}>Admin Page</Link> : (patientsMail.includes(user?.email) ? <Link style={{color: '#0f3460', fontWeight: 'bold', fontSize: '22px'}}>Patient</Link> : (doctorsMail.includes(user?.email) ? <Link style={{color: '#0f3460', fontWeight: 'bold', fontSize: '22px'}}>Doctor</Link> : null))
                 )}
               </li>
               <li>
